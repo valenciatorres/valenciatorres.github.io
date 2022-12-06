09fddf1188b933c94eaaeab8ffe9376abdfeff89
valenciatorres <119955312+valenciatorres@users.noreply.github.com>
2022-12-06 09:33:50 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+lines (11 sloc)  294 Bytes
+
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
